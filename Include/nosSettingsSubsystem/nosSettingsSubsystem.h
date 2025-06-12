6848f29c1168f3e72e7ab22227e4c0c6a0eaf0f4
Furkan Can TURAN <fcturan10@gmail.com>
2025-06-12 15:22:50 +0300

    Simplify design by merging editor item and settings entry concepts

--- Include/nosSettingsSubsystem/nosSettingsSubsystem.h
+++ Include/nosSettingsSubsystem/nosSettingsSubsystem.h
@@ -12,13 +12,18 @@ typedef enum nosSettingsFileDirectory {
 	NOS_SETTINGS_FILE_DIRECTORY_GLOBAL	// AppData folder
 } nosSettingsFileDirectory;
 
-typedef nosResult(*nosPfnSettingsItemUpdate)(const char* entryName, nosBuffer itemValue);
+typedef enum nosSettingsEntryType {
+	NOS_SETTINGS_ENTRY_TYPE_MODULE, // Module's settings
+	NOS_SETTINGS_ENTRY_TYPE_DEVICE // Device's settings
+} nosSettingsEntryType;
+
+typedef nosResult(*nosPfnSettingsEntryUpdate)(const char* entryName, nosBuffer itemValue);
+typedef uint64_t nosSettingsEntryId;
 
 #if NOS_HAS_CPLUSPLUS_20
-#include "EditorEvents_generated.h"
-typedef nos::sys::settings::editor::SettingsEditorItem nosSettingsEditorItem;
+typedef nos::fb::Visualizer nosSettingsEditorVisualizer;
 #else
-typedef void* nosSettingsEditorItem;
+typedef void* nosSettingsEditorVisualizer;
 #endif
 
 typedef struct nosSettingsEntryParams {
@@ -26,6 +31,10 @@ typedef struct nosSettingsEntryParams {
 	nosBuffer Buffer; // Don't forget to free this buffer
 	const char* EntryName; // If NULL, "default" will be used
 	nosSettingsFileDirectory Directory;
+	nosPfnSettingsEntryUpdate UpdateCallback; // Callback to update the entry value
+	nosSettingsEntryType Type; // Type of the entry
+	bool IsEditableFromEditor; // If true, this entry will be editable from editor
+	nosSettingsEditorVisualizer Visualizer; // Visualizer for the entry in editor
 } nosSettingsEntryParams;
 
 typedef struct nosSettingsSubsystem
@@ -33,24 +42,16 @@ typedef struct nosSettingsSubsystem
 	// parameters->directory will be set to found directory
 	// if parameters->typeName == 0, it will be set to first typename found in the closest file
 	// parameters->directory is ignored because closest one is chosen
-	nosResult(NOSAPI_CALL *ReadSettingsEntry)(nosSettingsEntryParams* parameters);
-	// parameters->typeName should be a valid typename
-	// if parameters->directory is not set, it will be set to NOS_SETTINGS_FILE_DIRECTORY_LOCAL
-	nosResult(NOSAPI_CALL *WriteSettingsEntry)(const nosSettingsEntryParams* parameters);
-
-
-	// Editor related functions
-
-	nosResult(NOSAPI_CALL *RegisterEditorSettings)(u64 itemCount, const nosSettingsEditorItem** itemList, nosPfnSettingsItemUpdate itemUpdateCallback, nosSettingsFileDirectory saveDirectory);
-	nosResult(NOSAPI_CALL *UnregisterEditorSettings)();
+	nosResult(NOSAPI_CALL *RegisterSettingsEntry)(nosSettingsEntryParams* parameters, nosSettingsEntryId* entryId);
+	void(NOSAPI_CALL* UnregisterSettingsEntry)(nosSettingsEntryId entryId);
 } nosSettingsSubsystem;
 
 #pragma region Helper Declarations & Macros
 // Make sure these are same with nossys file.
 #define NOS_SETTINGS_SUBSYSTEM_NAME "nos.sys.settings"
 
-#define NOS_SETTINGS_SUBSYSTEM_VERSION_MAJOR 0
-#define NOS_SETTINGS_SUBSYSTEM_VERSION_MINOR 10
+#define NOS_SETTINGS_SUBSYSTEM_VERSION_MAJOR 1
+#define NOS_SETTINGS_SUBSYSTEM_VERSION_MINOR 0
 
 extern struct nosPluginInfo nosSettingsModuleInfo;
 extern nosSettingsSubsystem* nosSettings;
